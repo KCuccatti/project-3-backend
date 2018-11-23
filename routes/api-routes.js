affe4418f9d6eeb154ba15e39fa4b69676e8a6fd
Kyle Cuccatti <kylecuccatti@ymail.com>
2018-11-23 12:33:04 -0600

    Added questions

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,7 +6,7 @@ var cors = require('cors');
 
 // Allows routes to be used outside of this file
 module.exports = function (myApp) {
-    
+
     var myFunctions = require('../models/index.js');
     var url = myFunctions.getUrl();
 
@@ -23,7 +23,7 @@ module.exports = function (myApp) {
 
         db.sequelize.query(userQuery,
             {
-                replacements: { username: req.params.username, password: req.params.password, srcSystemCode: req.params.srcSystemCode }, 
+                replacements: { username: req.params.username, password: req.params.password, srcSystemCode: req.params.srcSystemCode },
                 type: db.sequelize.QueryTypes.SELECT
             }
         ).then(function (response) {
@@ -52,7 +52,7 @@ module.exports = function (myApp) {
             }
         ).then(function (response) {
             console.log(response);
-            res.json({data:response});
+            res.json(response);
         }).catch(function (error) {
             console.log("AN ERROR OCCURED WHILE FETCHING CATEGORY INFO.");
             console.log(error);
@@ -62,4 +62,41 @@ module.exports = function (myApp) {
 
 
 
+
+    // Gets questions from db based on category
+    myApp.get("/api/GetQuestions/:categoryId", function (req, res) {
+
+        let questionQuery = 'SELECT  Q.id, Q.question_text, C.choice, Q.answer FROM question Q INNER JOIN  choices C ON Q.id = C.question_id WHERE Q.category_id = :categoryId';
+
+        db.sequelize.query(questionQuery,
+            {
+                replacements: { categoryId: req.params.categoryId },
+                type: db.sequelize.QueryTypes.SELECT
+            }
+        ).then(function (response) {
+            let id = '';
+            let questions = [];
+            let choices = [];
+            id = response[0].id;
+
+            for (let i = 0; i < response.length; i++) {
+                if (response[i].id === id) {
+                    // we are on the same question
+                    choices.push(response[i].choice);
+                } else {
+                    // We are on a new question
+                    questions.push(response[i-1].question_text, choices, response[i-1].answer);
+                    choices = [];
+                    choices.push(response[i].choice);
+                }
+                id = response[i].id;
+            }
+            res.json(questions);
+        }).catch(function (error) {
+            console.log("AN ERROR OCCURED WHILE FETCHING Questions INFO.");
+            console.log(error);
+            res.json({ error: error });
+        });
+
+    });
 }
